9004e63aad57852f19f6a5c37f375e3dce4b5811
Henry Lee <henryleeworld@gmail.com>
2020-05-15 07:06:38 +0800

    feat:初始化

---
+++ app/Http/Controllers/WalletController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+class WalletController extends Controller
+{
+    public function index()
+    {
+        $user = User::find(1);
+        $digitalWalletAry = [
+            'name' => 'Digital Wallet',
+            'slug' => 'digital-wallet',
+            'text' => '數位錢包',
+        ];
+        $paperWalletAry = [
+            'name' => 'Paper Wallet',
+            'slug' => 'paper-wallet',
+            'text' => '紙本錢包',
+        ];
+        $digitalWallet = $user->getWallet($digitalWalletAry['slug']);
+        if (!$user->hasWallet($digitalWalletAry['slug'])) {
+            $digitalWallet = $user->createWallet([
+                'name' => $digitalWalletAry['name'],
+                'slug' => $digitalWalletAry['slug'],
+            ]);
+        }
+        echo $digitalWalletAry['text'] . '儲值：$400' . PHP_EOL;
+        $digitalWallet->deposit(400);
+        echo $digitalWalletAry['text'] . '餘額：$' . $digitalWallet->balance . PHP_EOL;
+        echo $digitalWalletAry['text'] . '領出：$100' . PHP_EOL;
+        $digitalWallet->withdraw(100);
+        echo $digitalWalletAry['text'] . '餘額：$' . $digitalWallet->balance . PHP_EOL;
+        $paperWallet = $user->getWallet($paperWalletAry['slug']);
+        if (!$user->hasWallet($paperWalletAry['slug'])) {
+            $paperWallet = $user->createWallet([
+                'name' => $paperWalletAry['name'],
+                'slug' => $paperWalletAry['slug'],
+            ]);
+        }
+        echo $paperWalletAry['text'] . '餘額：$' . $paperWallet->balance . PHP_EOL;
+        echo $digitalWalletAry['text'] . '轉帳到' . $paperWalletAry['text'] . '：$150' . PHP_EOL;
+        $digitalWallet->transfer($paperWallet, 150);
+        echo $digitalWalletAry['text'] . '餘額：$' . $digitalWallet->balance . PHP_EOL;
+        echo $paperWalletAry['text'] . '餘額：$' . $paperWallet->balance . PHP_EOL;
+    }
+}
---
+++ app/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Bavix\Wallet\Traits\HasWallet;
+use Bavix\Wallet\Traits\HasWallets;
+use Bavix\Wallet\Interfaces\Wallet;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable implements Wallet
+{
+    use Notifiable, HasWallet, HasWallets;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
